6f2aa2fa4a96e3670914465a6fa02f541973ed6d
Andrzej Nagórko <amn@mimuw.edu.pl>
2018-02-28 06:57:29 +0100

    Punkty, proste i okręgi

---
+++ geom/geom.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+// punkty
+//   tworzenie
+//   środek
+//   odległość
+
+// proste
+//   tworzenie z równania
+//   tworzenie z dwóch punktów
+//   przecięcie
+
+// okręgi
+//   tworzenie z trzech punktów
+//   tworzenie ze środka i promienia
+//   wyznaczanie środka
+//   wyznaczanie promienia
+//   wyznaczanie równania
+
+class Point
+{
+public:
+	Point(float, float);
+};
+
+class Line
+{
+public:
+	Line(float A, float B, float C);
+	Line(Point A, Point b);
+
+};
+
+Point mid(Point a, Point b)
+{
+}
+
+class
+
+int main()
+{
+	return 0;
+}
+
