3509f15a3bd48234e0c15c421882a5a52b76b114
anagorko <amn@mimuw.edu.pl>
2018-04-04 16:46:19 +0200

    Update figures.cpp

--- abc/figures.cpp
+++ abc/figures.cpp
@@ -5,20 +5,20 @@ using namespace std;
 class Figure
 {
     virtual ostream& output(ostream&) const = 0;
-    
+
 public:
     virtual double area() = 0;
-    
+
     virtual Figure* scale(double) = 0;
     virtual Figure* translate(double, double) = 0;
     virtual Figure* rotate(double) = 0;
-    
-    friend ostream& operator<<(ostream& os, const Figure& f);
+
+    friend ostream& operator<<(ostream& os, const Figure* f);
 };
 
-ostream& operator<<(ostream& os, const Figure& f)
+ostream& operator<<(ostream& os, const Figure* f)
 {
-    return f.output(os);
+    return f -> output(os);
 }
 
 class Point : public Figure
@@ -28,14 +28,14 @@ class Point : public Figure
     virtual ostream& output(ostream& os) const {
         return os << "(" <<  x << "," << y << ")";
     }
-        
+
 public:
     Point() : Point(0.0, 0.0) {
     }
     Point(double _x, double _y) {
         x = _x; y = _y;
     }
-    
+
     virtual double area() {
         return 0;
     }
@@ -43,35 +43,35 @@ public:
     virtual Figure* scale(double k) {
         x = k * x;
         y = k * y;
-        
+
         return this;
     };
-    
+
     virtual Figure* translate(double dx, double dy) {
         x = x + dx;
         y = y + dy;
 
         return this;
     }
-    
+
     virtual Figure* rotate(double alpha) {
         double nx = cos(alpha) * x - sin(alpha) * y;
         double ny = sin(alpha) * x + cos(alpha) * y;
-        
+
         x = nx;
         y = ny;
-        
+
         return this;
     }
-    
+
     double distanceTo(Point p)
     {
         double dx = x - p.x;
         double dy = y - p.y;
-        
+
         return sqrt(dx*dx + dy*dy);
     }
-    
+
     double getX() const { return x; }
     double getY() const { return y; }
 };
@@ -80,30 +80,30 @@ public:
 class Square : public Figure
 {
     Point a,b,c,d;
-    
+
     virtual ostream& output(ostream& os) const {
-        return os << "[" << a << ";" << b << ";" << c << ";" << d << "]";
+        return os << "[" << &a << ";" << &b << ";" << &c << ";" << &d << "]";
     }
-        
+
 public:
     Square(Point p, double x) {
         a = b = c = d = p;
-        
+
         b.translate(x,0);
         c.translate(x,x);
         d.translate(0,x);
     }
-    
+
     virtual double area() {
         return a.distanceTo(b) * a.distanceTo(b);
     }
-    
+
     virtual Figure* scale(double k) {
         a.scale(k);
         b.scale(k);
         c.scale(k);
-        d.scale(k);        
-        
+        d.scale(k);
+
         return this;
     }
 
@@ -121,7 +121,7 @@ public:
         b.rotate(alpha);
         c.rotate(alpha);
         d.rotate(alpha);
-        
+
         return this;
     }
 };
@@ -131,12 +131,12 @@ int main(int argc, char ** argv)
     Figure* f = new Square(Point(10.0, 20.0), 30.0);
     Figure* g = new Point(-5.0, 15.0);
 
-    cout << *f << " ma pole " << f -> area() << endl;    
-    cout << *g << " ma pole " << g -> area() << endl;    
+    cout << f << " ma pole " << f -> area() << endl;
+    cout << g << " ma pole " << g -> area() << endl;
 
     // Napisz klasy Circle i Line powyżej tak, by poniższy kod się kompilował
 
-/*    
+/*
     Figure* c = new Circle(Point(40.0, 50.0), 5.0);
     c -> translate(10.0, 5.0);
     c -> rotate(0.5);
@@ -145,6 +145,6 @@ int main(int argc, char ** argv)
     Figure* l = new Line(Point(5.0, 7.0), Point(3.0, 2.0));
     l -> rotate(-0.2);
     l -> translate(3.0, 5.0);
-    cout << *l << endl;    
+    cout << *l << endl;
 */
 }
